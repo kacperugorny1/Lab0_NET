14647cca6b6a19be2aa674818d72ee6a7f920cea
kacugo0599 <kacugo0599@W04N-P317D10>
2024-02-28 19:39:21 +0100

    Add project files.

---
+++ Lab0_NET/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0_NET
+{
+    internal class FizzBuzz
+    {
+        int uB;
+        public FizzBuzz(int uB) { this.uB = uB; }
+        public void Display()
+        {
+            for(int i = 1; i <= uB; i++)
+            {
+                string str = "";
+                if (i % 3 == 0) str += "Fizz";
+                if (i % 5 == 0) str += "Buzz";
+                if (str == "") Console.WriteLine(i);
+                else Console.WriteLine(str);
+            }
+        }
+    } 
+}
---
+++ Lab0_NET/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab0_NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fz1 = new(100);
+            fz1.Display();
+        }
+    }
+}
